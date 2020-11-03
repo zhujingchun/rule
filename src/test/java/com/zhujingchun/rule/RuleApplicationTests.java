abb3c76d8812c666aff25f59c6976cb2aec89f98
zhujingchun <zhujingchun@edianzu.com>
2020-11-03 17:34:56 +0800

    init

---
+++ src/test/java/com/zhujingchun/rule/RuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhujingchun.rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
